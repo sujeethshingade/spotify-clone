0e22bdada4f367476a9a3202936ac46002955657
Sujeeth Shingade <sujeethshingade04@gmail.com>
2024-09-09 21:29:09 +0530

    add header

--- components/Library.tsx
+++ components/Library.tsx
@@ -12,7 +12,7 @@ const Library = () => {
         <div className="flex flex-col">
             <div className="flex items-center justify-between px-5 pt-4">
                 <div className="inline-flex items-center gap-x-2">
-                    <TbPlaylist className="text-neutral-400" size={26} />
+                    <TbPlaylist className="text-neutral-400" size={24} />
                     <p className="text-neutral-400 font-medium text-md">
                         Your Library
                     </p>
