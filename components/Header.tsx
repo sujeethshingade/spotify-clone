c868fadb646e4569c60d64bbd61b9259e319dc38
Sujeeth Shingade <sujeethshingade04@gmail.com>
2024-09-09 23:45:48 +0530

    add buttons

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { RxCaretLeft, RxCaretRight } from 'react-icons/rx';
 import { twMerge } from 'tailwind-merge';
 import { HiHome } from 'react-icons/hi';
 import { BiSearch } from 'react-icons/bi';
+import Button from './Button';
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -37,7 +38,22 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
                         <BiSearch className='text-black' size={20} />
                     </button>
                 </div>
+                <div className='flex justify-between items-center gap-x-4'>
+                    <>
+                        <div>
+                            <Button onClick={() => { }} className='bg-transparent text-neutral-300 font-medium'>
+                                Sign Up
+                            </Button>
+                        </div>
+                        <div>
+                            <Button onClick={() => { }} className='bg-white px-6 py-2'>
+                                Login
+                            </Button>
+                        </div>
+                    </>
+                </div>
             </div>
+            {children}
         </div>
     );
 }
